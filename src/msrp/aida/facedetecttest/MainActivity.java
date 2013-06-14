5d14a047eb0debe8a606518af0ea815e66e69032
Jose Acevedo <acevedoflores.jose@gmail.com>
2013-06-14 16:42:40 -0400

    FaceDetection Api test is working*
    It works only on certain orietations.
    Example:
    If camera.setDosplayOrientation(90) has paremeter 90, face detection
    doesn't work on portrait mode, only on landscape.
    If the paremeter is left at 0, face detection works in landscape but
    not in portrait.

--- src/msrp/aida/facedetecttest/MainActivity.java
+++ src/msrp/aida/facedetecttest/MainActivity.java
@@ -1,15 +1,33 @@
 package msrp.aida.facedetecttest;
 
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.Face;
+import android.hardware.Camera.FaceDetectionListener;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+
+	private Camera mCamera;
+	private CameraPreview mPreview;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.camera_preview);
+		// Create an instance of Camera
+		mCamera = getCameraInstance();
+		mCamera.setFaceDetectionListener(new FaceDetect());
+	//	mCamera.setDisplayOrientation(90);
+		Button start_detect = (Button) findViewById(R.id.start_detect);
+		start_detect.setOnClickListener(this);
 	}
 
 	@Override
@@ -19,4 +37,58 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		if (mCamera != null){
+			mCamera.release();        // release the camera for other applications
+			mCamera = null;
+		}            
+	}
+
+
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance(){
+		Camera c = null;
+		try {
+			c = Camera.open(CameraInfo.CAMERA_FACING_FRONT); // attempt to get a Camera instance
+		}
+		catch (Exception e){
+			// Camera is not available (in use or does not exist)
+		}
+		return c; // returns null if camera is unavailable
+	}
+
+	@Override
+	public void onClick(View v) {
+		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview3);
+		// Create our Preview view and set it as the content of our activity.
+		mPreview = new CameraPreview(this, mCamera);
+
+
+		System.out.println(preview);
+
+		preview.addView(mPreview);
+		
+	}
 }
+/**
+ * 
+ * @author joseacevedo
+ *
+ */
+class FaceDetect implements FaceDetectionListener{
+
+	@Override
+	public void onFaceDetection(Face[] faces, Camera camera) {
+		if(faces.length >= 1)
+			Log.d("FaceDetectTest", "face detected: "+ faces.length +
+					" Face 1 Location X: " + faces[0].rect.centerX() +
+					"Y: " + faces[0].rect.centerY() );
+		else
+			Log.d("FaceDetectTest", "Wu " + faces.length);
+	}
+
+}
+
